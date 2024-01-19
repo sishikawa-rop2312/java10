0784343f5680db8fbda47c82b1de6503204c346f
sishikawa-rop2312 <tukuyomi610315@gmail.com>
2024-01-19 19:58:09 +0900

    fist commit

---
+++ src/java10/Matango.java
@@ -0,0 +1,11 @@
+package java10;
+
+public class Matango {
+	int hp;
+	final int LEVEL = 10;
+	char suffix;
+	
+	public void run() {
+		System.out.printf("お化けキノコは%c逃げ出した\n", this.suffix);
+	}
+}
---
+++ src/java10/SuperHero.java
@@ -0,0 +1,17 @@
+package java10;
+
+public class SuperHero extends Hero {
+	boolean flying;
+
+	// 飛ぶ
+	public void fly() {
+		this.flying = true;
+		System.out.println("飛び上がった！");
+	}
+
+	// 着地する
+	public void land() {
+		this.flying = false;
+		System.out.println("着地した！");
+	}
+}
